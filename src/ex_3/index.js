978c0529cc11c06178523372438b88d24091827f
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-03-08 20:42:44 -0300

    Aula Native 08/03

--- src/ex_3/index.js
+++ src/ex_3/index.js
@@ -1,13 +1,57 @@
+import React, { useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
 
 export default function Exemplo_3 (){
+
+    const [numero, setNumero] = useState(3);
+    //let numero = 0;
+    // setNumero é o que garante a chamada dentro do array??*********
+
+    function Ola (nome) {
+        alert (`Olá ${nome}`);
+        // quando há soma/junção para formação da frase usamos a concatenação
+    }
+
+    {/* Para não usar o return usamos o {} ao final da frase daa function */}
+
+    function incremento () {
+        numero++;
+        console.log(numero);
+    }
+
     return (
         <View style = {styles.container} >
-            <Text>
+            <Text style = {styles.titulo}>
                 Exemplo_3
             </Text>
+            <TouchableOpacity 
+                style ={ styles.botao} 
+                onPress={(Ola) => Ola('Sayuri')}
+            >
+            {/* => Dear Function, usado para "chamar uma function dentro da outra?"************** */}
+            {/* Podemos quebrar a linha a partir de cada "função" chamada, neste caso o style e o OnPress */}
+            {/* OnPress é o msm que OnClick, o () é o parametro e o {} é onde seria inserido o codigo */}
+
+            <Text style ={styles.txtBotao}> 
+                Botão
+            </Text>
+            </TouchableOpacity>
+
+            <Text style ={styles.titulo}> 
+                Valor: {numero} 
+            </Text>
+            
+            <TouchableOpacity
+            style ={ styles.botao} 
+            onPress={() => {}}
+            >
+            <Text style ={styles.txtBotao}> 
+                +1
+            </Text>
+            </TouchableOpacity>
+
         </View>
     );
 }
--- src/ex_3/styles.js
+++ src/ex_3/styles.js
@@ -3,10 +3,33 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 8,
         width: '100%',
         alignItems: 'center',
+        borderRadius: 20,
         backgroundColor: '#f5f5f5',
-    }
+    },
+
+    titulo: {
+        margin: 8,
+        fontSize: 20,
+        marginBottom: 20,
+        fontWeight: 'bold',
+    },
+
+    botao: {
+        margin: 18,
+        padding: 16,
+        width: '70%',
+        borderRadius: 20,
+        alignItems: 'center',
+        backgroundColor: '#000',
+    },
+
+    txtBotao: {
+        fontSize: 20,
+        color: '#f5f5f5',
+    },
 });
 
 export default styles;
